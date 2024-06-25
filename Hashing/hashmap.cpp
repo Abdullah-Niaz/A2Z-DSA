b8aa1b7b0334d54c3ba3df15b227dd62486afa92
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-06-26 00:32:15 +0500

    hashmap

---
+++ Hashing/hashmap.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n ;
+    cout<<"Enter the Size of Array: ";
+    cin>>n;
+    int arr[n];
+    map<int,int> ma;
+    // map<map of int, storing it's frequency> 
+    // map<key, value>
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+        ma[arr[i]]++;
+    }
+    
+    int q;
+    cout<<"Enter the Number of Queries: ";
+    cin>>q;
+    while (q--)
+    {
+        int number;
+        std::cout << "Enter the Queuery: " << std::endl;
+        cin>>number;
+        std::cout << ma[number] << std::endl;
+    }
+    
+    
+    return 0;
+}
